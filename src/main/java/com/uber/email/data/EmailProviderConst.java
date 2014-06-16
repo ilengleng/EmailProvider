89be92a344fdf05011d8bde431225dee2902be85
lichen <jenny.chen0603@gmail.com>
2014-06-15 22:07:07 -0700

    fix a typo

--- src/main/java/com/uber/email/data/EmailProviderConst.java
+++ src/main/java/com/uber/email/data/EmailProviderConst.java
@@ -1,7 +1,7 @@
 package com.uber.email.data;
 
 public interface EmailProviderConst {
-	public static final String EMAIL_PROVIDER_PROP = "mailprovider";
+	public static final String EMAIL_PROVIDER_PROP = "emailprovider";
 	public static final String MAILGUN_API_ENDPOINT = "https://api.mailgun.net/v2/sandbox82ba54d7862441c88e5db939de0f23a8.mailgun.org/messages";
 	public static final String MANDRILL_API_ENDPOINT = "https://mandrillapp.com/api/1.0/messages/send.json";
 
